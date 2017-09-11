426d1cb161e4c5fd03cb92372291772e9a536bf6
Вяткин Никита Алексеевич <navyatkin@edu.hse.ru>
2017-09-12 02:16:14 +0500

    Добавлен весь функционал. Программа работает. Некорректно.

---
+++ First/First/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State start = State.Read();
+            State finish = State.Read();
+            Dictionary<State, int> len = new Dictionary<State, int>();
+            Queue<State> q = new Queue<State>();
+            len[start] = 0;
+            q.Enqueue(start);
+            FileStream fs = new FileStream("OUTPUT.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            int r = 0;
+            while (q.Count != 0)
+            {
+               
+                State cur = q.Dequeue();
+                if (cur == finish)
+                {                    
+                    sw.Write(len[cur]);
+                    Console.ReadLine();
+                    sw.Close();
+                    fs.Close();
+                    Environment.Exit(0);
+                }
+                for (int di = -1; di <= 1; di++)
+                {
+                    for(int dj = -1; dj <= 1; dj++)
+                    {
+                        if (di * di + dj * dj == 1)
+                        {
+                            State next = cur.Shift(di,dj);
+                            if (!len.ContainsKey(next))
+                            {
+                                len[next] = len[cur] + 1;                                                            
+                                q.Enqueue(next);
+                                
+                            }
+                            r++;
+                        }
+                    }
+                }
+            }
+            sw.Write(-1);
+            Console.WriteLine(r);
+            sw.Close();
+            fs.Close();
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ First/First/State.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace First
+{
+    class State
+    {
+        char[,] a = new char[2, 4];
+        public State(State t)
+        {
+            a = t.a;
+        }
+        public State()
+        {
+            a = new char[2, 4];
+        }       
+        static FileStream fs = new FileStream("INPUT.txt", FileMode.Open);
+        static StreamReader sr = new StreamReader(fs);
+        public static State Read()
+        {
+            State s = new State();
+            string str = "";            
+            for (int i = 0; i < 2; i++)
+            {
+                str = sr.ReadLine();
+                for (int j = 0; j < 4; j++)
+                {
+                    s.a[i, j] = str[j];
+                    Console.Write(s.a[i, j]);
+
+                }
+                
+            }
+            Console.WriteLine();       
+            return s;
+        }
+        public State Shift(int di, int dj)
+        {
+            State next = this;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (a[i, j] == '#')
+                    {
+                        int ni = i + di;
+                        int nj = j + dj;
+                        if (0 <= ni && ni < 2 && 0 <= nj && nj < 4)
+                        {
+                            char t = next.a[i, j];
+                            next.a[i, j] = next.a[ni, nj];
+                            next.a[ni, nj] = t;
+                        }
+                        return next;
+                    }
+                }
+            }
+            return next;
+
+        }
+        static public int Compare (State left, State right)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (left.a[i, j] != right.a[i, j]) { return -1; }
+                }
+            }
+            return 0;
+        }
+        //static public bool operator <(State left, State right)
+        //{
+        //    return Compare(left, right) < 0;
+        //}
+        //static public bool operator >(State left, State right)
+        //{
+        //    return Compare(left, right) > 0;
+        //}
+        static public bool operator ==(State left, State right)
+        {
+            return Compare(left, right) == 0;
+        }
+        static public bool operator !=(State left, State right)
+        {
+            return Compare(left, right) != 0;
+        }
+    }
+}
